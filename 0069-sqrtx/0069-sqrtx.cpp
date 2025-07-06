98fe88fd760016373320698d93a9a0707f4ef8dc
Samy Tamim <samytamim0@gmail.com>
2025-07-06 14:55:36 +0100

    Time: 1 ms (22.64%), Space: 8.6 MB (50.9%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long ret = 0;
-        while (ret * ret < x) {
-            ret++;
+        long l = 0;
+        long m = x / 2;
+        long h = x;
+
+        if (x == 1)
+            return 1;
+
+        while (m * m != x) {
+            if (m * m > x) {
+                h = m;
+                m -= (h - l) / 2;
+            } else if (m * m < x) {
+                l = m;
+                m += (h - l) / 2;
+            }
+            if (h - l == 1 || l == h) {
+                m = l;
+                break;
+            }
         }
-        return ret * ret == x ? ret : ret - 1;
+
+        return m;
     }
 };
