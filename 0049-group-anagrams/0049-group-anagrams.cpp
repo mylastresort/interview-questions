aa0db6ba7af1dd419652c7ce97fe8062e68c83a2
Samy Tamim <samytamim0@gmail.com>
2025-07-09 21:04:04 +0100

    Time: 21 ms (38.22%), Space: 25.4 MB (61.36%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>*> map;
+
+        vector<vector<string>> ret;
+
+        for (auto s : strs) {
+            string c(s);
+
+            sort(c.begin(), c.end());
+
+            if (map.count(c)) {
+                map[c]->push_back(s);
+                cout << c << " " << s << endl;
+            } else {
+                vector<string> *grp = new vector<string>();
+                map[c] = grp;
+                grp->push_back(s);
+            }
+        }
+
+        for (auto grp: map) {
+            ret.push_back(*grp.second);
+        }
+
+        return ret;
+    }
+};
